78cf19e7130c1341853a2ba04aa55000ef9e77b3
Guilherme Rigotti <ggrigotti@gmail.com>
2021-01-22 16:56:54 -0300

    tests: finish tests

--- test/ex_mon_test.exs
+++ test/ex_mon_test.exs
@@ -20,13 +20,45 @@ defmodule ExMonTest do
     test "when the game started, returns a message" do
       player = Player.build("Guilherme", :chute, :cura, :soco)
 
-      messages = capture_io(fn ->
-        assert ExMon.start_game(player) == :ok
-      end)
+      messages =
+        capture_io(fn ->
+          assert ExMon.start_game(player) == :ok
+        end)
 
       assert messages =~ "The game is started"
       assert messages =~ "status: :started"
       assert messages =~ "turn: :player"
+    end
+  end
+
+  describe "make_move/1" do
+    setup do
+      player = Player.build("Guilherme", :chute, :cura, :soco)
+
+      capture_io(fn ->
+        ExMon.start_game(player)
+      end)
+
+      :ok
+    end
+
+    test "when the move is valid, do the move and the computer makes a move" do
+      messages = capture_io(fn ->
+        ExMon.make_move(:chute)
+      end)
+
+      assert messages =~ "The Player attacked the computer"
+      assert messages =~ "It's computer turn"
+      assert messages =~ "It's player turn"
+      assert messages =~ "status: :continue"
+    end
+
+    test "when the move is invalid, returns a error message" do
+      messages = capture_io(fn ->
+        ExMon.make_move(:wrong)
+      end)
+
+      assert messages =~ "Invalid move: wrong"
     end
   end
 end
